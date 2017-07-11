2442a0f102326d7ad1fd502c1ce606829d1e8563
Nic Horstmeier <nhorstmeier@gmail.com>
2017-07-11 07:36:45 -0500

    ENHANCEMENT MailChimpSubscriberForm tests

--- tests/MailChimpSubscriberFormTest.php
+++ tests/MailChimpSubscriberFormTest.php
@@ -18,22 +18,6 @@ class MailChimpSubscriberFormTest extends SapphireTest
         'MailChimpTestPage',
     );
 
-    public function setUp()
-    {
-        parent::setUp();
-
-        /*$form = MailChimpTestSubscriberForm::create();
-        $form->Title = 'Form One';
-        $form->ListID = '76ac48ccc2';
-        $form->write();
-
-        $page = MailChimpTestPage::create();
-        $page->Title = 'Test MailChimp Page';
-        $page->MailChimpFormID = $form->ID;
-        $page->writeToStage('Stage');
-        $page->doPublish();*/
-    }
-
     /**
      *
      */
@@ -70,6 +54,90 @@ class MailChimpSubscriberFormTest extends SapphireTest
         $newForm->write();
 
         $this->assertTrue($newForm->canDelete($this->objFromFixture('Member', 'cancreate')));
+    }
+
+    /**
+     *
+     */
+    public function testCanView()
+    {
+        $this->assertTrue(Injector::inst()->get('MailChimpSubscriberForm')->canView());
+    }
+
+    /**
+     *
+     */
+    public function testGetCMSFields()
+    {
+        /*$newFields = Injector::inst()->get('MailChimpSubscriberForm')->getCMSFields();
+
+        $this->assertNull($newFields->dataFieldbyName('FieldsToShow'));
+        $this->assertNull($newFields->dataFieldbyName('InterestCategoriesToShow'));
+        $this->assertNull($newFields->dataFieldbyName('Pages'));
+
+        $existing = $this->objFromFixture('MailChimpTestSubscriberForm', 'one');
+        $fields   = $existing->getCMSfields();
+
+        $this->assertInstanceOf('CheckboxsetField', $fields->dataFieldbyName('FieldsToShow'));
+        $this->assertInstanceOf('CheckboxsetField', $fields->dataFieldbyName('InterestCategoriesToShow'));
+        $this->assertInstanceOf('GridField', $fields->dataFieldbyName('Pages'));
+        //*/
+    }
+
+    /**
+     *
+     */
+    public function testGetCMSValidator()
+    {
+        $this->assertInstanceOf('RequiredFields', Injector::inst()->get('MailChimpSubscriberForm')->getCMSValidator());
+        $this->assertTrue(Injector::inst()->get('MailChimpSubscriberForm')->getCMSValidator()->fieldIsRequired('ListID'));
+    }
+
+    /**
+     *
+     */
+    public function testGetMergeFormFields()
+    {
+        /*$form        = $this->objFromFixture('MailChimpTestSubscriberForm', 'one');
+        $mergeFields = $form->getMergeFormFields();
+
+        $this->assertTrue(is_array($mergeFields));
+        $this->assertEquals(2, count($mergeFields));
+
+        foreach ($mergeFields as $field) {
+            $this->assertInstanceOf('FormField', $field);
+        }
+        //*/
+    }
+
+    /**
+     *
+     */
+    public function testGetInterestCategoryFields()
+    {
+        /*$form           = $this->objFromFixture('MailChimpTestSubscriberForm', 'one');
+        $interestFields = $form->getInterestCategoryFields();
+
+        $this->assertTrue(is_array($interestFields));
+        $this->assertEquals(3, count($interestFields));
+
+        foreach ($interestFields as $field) {
+            $this->assertInstanceOf('FormField', $field);
+        }
+        //*/
+    }
+
+    /**
+     *
+     */
+    public function testProvidePermissions()
+    {
+        $expected = [
+            'MailChimp_create' => 'Create MailChimp Form',
+            'MailChimp_edit'   => 'Edit MailChimp Form',
+            'MailChimp_delete' => 'Delete MailChimp Form',
+        ];
+        $this->assertEquals($expected, Injector::inst()->get('MailChimpSubscriberForm')->providePermissions());
     }
 
 }
